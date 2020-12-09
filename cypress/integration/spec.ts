1c92fa122eb6277aad06e2a8ac6b7b4a84a2be2b
filiphric <filip@slido.com>
2020-12-09 16:36:01 +0100

    add tests

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -8,5 +8,48 @@ it('pastes text to textarea', () => {
   cy
     .get('[type=\'email\']')
     .invoke('val', textToPaste)
+    .trigger('blur')
+
+});
+
+it('copies text from textarea (works in electron only)', () => {
+
+  const textToCopy = 'Wubba lubba dub dub';
+
+  cy
+    .visit('index.html');
+
+  cy
+    .get('#copyText')
+    .invoke('val', textToCopy)
+
+  cy
+    .get('button')
+    .click()
+
+  cy
+    .task('getClipboard')
+    .should('eq', textToCopy)
+
+});
+
+it('copies text from textarea (works with chrome)', () => {
+
+  const textToCopy = 'Wubba lubba dub dub';
+
+  cy
+    .visit('index.html');
+
+  cy
+    .get('#copyText')
+    .invoke('val', textToCopy)
+
+  cy
+    .get('#button')
+    .realClick()
+
+  cy
+    .task('getClipboard')
+    .should('eq', textToCopy)
 
 });
